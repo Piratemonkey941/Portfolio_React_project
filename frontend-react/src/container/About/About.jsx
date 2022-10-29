fc6e3f62eb9850bb0688125004a1a6801448b7a4
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2022-10-29 17:08:27 -0500

    About and header sections implimented

--- frontend-react/src/container/About/About.jsx
+++ frontend-react/src/container/About/About.jsx
@@ -1,15 +1,77 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
+import {motion} from 'framer-motion'
 import './About.scss'
+// import {AppWrap, MotionWrap} from '../../wrapper'
+// import {urlFor, client} from '../../client'
+import {images} from '../../constants'
+
+
+const abouts = [
+  {title: 'Front End Development', description: 'I am good at Front End Development', imgUrl: images.about01},
+  {title: 'Web Design', description: 'I am good at Web Design', imgUrl: images.about02},
+  {title: 'UI/UX', description: 'I am good at UI/UX', imgUrl: images.about03},
+  {title: 'Web Animations', description: 'I am good at Web Animations', imgUrl: images.about04},
+];
+
 
 const About = () => {
- 
+
 
   return (
-    <div>About</div>
+    <>
+    <h2 className='head-text'> I know that <span>Good Develpment</span> <br /> means <span>Good Buisness</span>  </h2>    
+      <div className='app__profiles'>
+        {abouts.map((about, index) => (
+          <motion.div
+          whileInView={{ opacity: 1 }}
+          whileHover={{ scale: 1.1 }}
+          transition={{ duration: 0.5, type: 'tween' }}
+          className='app__profile-item'
+          key={about.title + index}
+          >
+          <img src={about.imgUrl} alt={about.title} />
+          <h2 className='bold-text' style={{ marginTop: 20 }} >{about.title}</h2>
+          <p className='p-bold'style={{ marginTop: 10 }}>{about.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    </>
   );
 };
 
 export default About
 
 
+// const About = () => {
+//   const [about, setAbouts] = useState([])
+
+//   useEffect(() => {
+//     const query = '*[_type == "abouts"]';
+
+//     client.fetch(query).then((data) => {
+//       setAbouts(data)
+//     });
+//   }, []);
+
+// return (
+//   <>
+//   <h2 className='head-text'> I know that <span>Good Design</span> <br />means <span>Good Buisness</span></h2>
+  
+//     <div className='head-text'>
+//       {setAbouts.map((about, index) => (
+//         <motion.div
+//         whileInView={{ opacity: 1 }}
+//         whileHover={{ scale: 1.1 }}
+//         transition={{ duration: 0.5, type: 'tween' }}
+//         className='app__profile-item'
+//         key={about.title + index}
+//         >
+//         <img src={urlFor(about.imgUrl)} alt={about.title} />
+//         <h2 className='bold-text' style={{ marginTop: 20 }} >{about.title}</h2>
+//         <p className='p-bold'style={{ marginTop: 10 }}>{about.description}</p>
+//         </motion.div>
+//       ))}
+//     </div>
+//   </>
+// );
+// };
